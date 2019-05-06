0b34d143e9a1ec8893503cbed99775ae72bd7e9e
eyakub <eyakubsorkar@gmail.com>
2019-05-07 01:28:40 +0600

    Cart view...under progress...

--- src/ecommerce/carts/views.py
+++ src/ecommerce/carts/views.py
@@ -1,14 +1,19 @@
 from django.shortcuts import render
+from .models import Cart
 
 
 # Create your views here.
 def cart_home(request):
+    # del request.session['cart_id']
     cart_id = request.session.get('cart_id', None)
     if cart_id is None:     # and isinstance(cart_id, int):
-        print('create new cart')
-        request.session['cart_id'] = 12
+        cart_obj = Cart.objects.create(user=None)
+        request.session['cart_id'] = cart_obj.id
+        print('New Cart created')
     else:
         print('Card ID exist')
+        print(cart_id)
+        cart_obj = Cart.objects.get(id=cart_id)
     return render(request, "carts/home.html", {})
 
     # print(request.session)  # on the request

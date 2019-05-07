81a79ce705e0d1329f65423ddc2283aa727dd65f
Eyakub Sorkar <eyakub.tryonyx@gmail.com>
2019-05-07 16:00:52 +0600

    update cart if no authenticated user(cart model manager part 1)

--- src/ecommerce/carts/models.py
+++ src/ecommerce/carts/models.py
@@ -5,12 +5,24 @@ User = settings.AUTH_USER_MODEL
 
 
 # Create your models here.
+class CartManager(models.Manager):
+    def new(self, user=None):
+        print(user)
+        user_obj = None
+        if user is not None:
+            if user.is_authenticated:
+                user_obj = user
+        return self.model.objects.create(user=user_obj)
+
+
 class Cart(models.Model):
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, blank=True)
     total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
+
+    objects = CartManager()
 
     def __str__(self):
         return str(self.id)
--- src/ecommerce/carts/views.py
+++ src/ecommerce/carts/views.py
@@ -3,15 +3,9 @@ from .models import Cart
 
 
 # Create your views here.
-def cart_create(user=None):
-    cart_obj = Cart.objects.create(user=None)
-    print('new cart created')
-    return cart_obj
-
-
 def cart_home(request):
     # del request.session['cart_id']
-    request.session['cart_id'] = '12'
+    # request.session['cart_id'] = '12'
     cart_id = request.session.get('cart_id', None)
     # if cart_id is None:     # and isinstance(cart_id, int):
     #     cart_obj = cart_create()
@@ -20,9 +14,12 @@ def cart_home(request):
     qs = Cart.objects.filter(id=cart_id)
     if qs.count() == 1:
         print('cart ID exists')
-        cart_obj = qs.first()
+        cart_obj = qs.first()   # if this cart doesn't have any user
+        if request.user.is_authenticated and cart_obj.user is None:
+            cart_obj.user = request.user
+            cart_obj.save()
     else:
-        cart_obj = cart_create()
+        cart_obj = Cart.objects.new(user=request.user)  # this should handle user authenticated or not
         request.session['cart_id'] = cart_obj.id
     return render(request, "carts/home.html", {})
 

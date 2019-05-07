cabefc1d061e87af12e9febdb2efad90b4d82213
eyakub <eyakubsorkar@gmail.com>
2019-05-08 05:21:10 +0600

    m2m changed signal to calculate cart total

--- src/ecommerce/carts/models.py
+++ src/ecommerce/carts/models.py
@@ -1,13 +1,32 @@
 from django.db import models
 from django.conf import settings
 from ecommerce.products.models import Product
+from django.db.models.signals import pre_save, post_save, m2m_changed
 User = settings.AUTH_USER_MODEL
 
 
 # Create your models here.
 class CartManager(models.Manager):
+
+    def new_or_get(self, request):
+
+        cart_id = request.session.get('cart_id', None)
+        # print(cart_id)
+        qs = self.get_queryset().filter(id=cart_id)
+        if qs.count() == 1:
+            new_obj = False
+            cart_obj = qs.first()
+            if request.user.is_authenticated and cart_obj.user is None:
+                cart_obj.user = request.user
+                cart_obj.save()
+        else:
+            cart_obj = Cart.objects.new(user=request.user)
+            new_obj = True
+            request.session['cart_id'] = cart_obj.id
+        return cart_obj, new_obj
+
     def new(self, user=None):
-        print(user)
+        # print(user)
         user_obj = None
         if user is not None:
             if user.is_authenticated:
@@ -18,6 +37,7 @@ class CartManager(models.Manager):
 class Cart(models.Model):
     user = models.ForeignKey(User, null=True, blank=True, on_delete=models.CASCADE)
     products = models.ManyToManyField(Product, blank=True)
+    subtotal = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
     total = models.DecimalField(default=0.00, max_digits=100, decimal_places=2)
     updated = models.DateTimeField(auto_now=True)
     timestamp = models.DateTimeField(auto_now_add=True)
@@ -26,3 +46,26 @@ class Cart(models.Model):
 
     def __str__(self):
         return str(self.id)
+
+
+def m2m_changed_cart_receiver(sender, instance, action, *args, **kwargs):
+    # print(action)
+    if action == 'post_add' or action == 'post_remove' or action == 'post_clear':
+        products = instance.products.all()
+        total = 0
+        for x in products:
+            total += x.price
+        if instance.subtotal != total:
+            instance.subtotal = total
+            instance.save()
+
+
+m2m_changed.connect(m2m_changed_cart_receiver, sender=Cart.products.through)
+
+
+# Pre_save Action
+def pre_save_cart_receiver(sender, instance, *args, **kwargs):
+    instance.total = instance.subtotal
+
+
+pre_save.connect(pre_save_cart_receiver, sender=Cart)
--- src/ecommerce/carts/views.py
+++ src/ecommerce/carts/views.py
@@ -4,23 +4,31 @@ from .models import Cart
 
 # Create your views here.
 def cart_home(request):
+    cart_obj, new_obj = Cart.objects.new_or_get(request)
+    products = cart_obj.products.all()
+    total = 0
+    for x in products:
+        total += x.price
+    print(total)
+    cart_obj.total = total
+    cart_obj.save()
     # del request.session['cart_id']
     # request.session['cart_id'] = '12'
-    cart_id = request.session.get('cart_id', None)
-    # if cart_id is None:     # and isinstance(cart_id, int):
-    #     cart_obj = cart_create()
-    #     request.session['cart_id'] = cart_obj.id
+    # cart_id = request.session.get('cart_id', None)
+    # # if cart_id is None:     # and isinstance(cart_id, int):
+    # #     cart_obj = cart_create()
+    # #     request.session['cart_id'] = cart_obj.id
+    # # else:
+    # qs = Cart.objects.filter(id=cart_id)
+    # if qs.count() == 1:
+    #     print('cart ID exists')
+    #     cart_obj = qs.first()   # if this cart doesn't have any user
+    #     if request.user.is_authenticated and cart_obj.user is None:
+    #         cart_obj.user = request.user
+    #         cart_obj.save()
     # else:
-    qs = Cart.objects.filter(id=cart_id)
-    if qs.count() == 1:
-        print('cart ID exists')
-        cart_obj = qs.first()   # if this cart doesn't have any user
-        if request.user.is_authenticated and cart_obj.user is None:
-            cart_obj.user = request.user
-            cart_obj.save()
-    else:
-        cart_obj = Cart.objects.new(user=request.user)  # this should handle user authenticated or not
-        request.session['cart_id'] = cart_obj.id
+    #     cart_obj = Cart.objects.new(user=request.user)  # this should handle user authenticated or not
+    #     request.session['cart_id'] = cart_obj.id
     return render(request, "carts/home.html", {})
 
     # print(request.session)  # on the request

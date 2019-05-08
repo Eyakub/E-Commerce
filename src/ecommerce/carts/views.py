c75fc1e99f9618572cfc88af7feae37e1800e993
Eyakub Sorkar <eyakub.tryonyx@gmail.com>
2019-05-08 13:34:33 +0600

    actually cart removing was working but product_id was fixed

--- src/ecommerce/carts/views.py
+++ src/ecommerce/carts/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from .models import Cart
 from ecommerce.products.models import Product
 
+
 # Create your views here.
 def cart_home(request):
     cart_obj, new_obj = Cart.objects.new_or_get(request)
@@ -44,7 +45,7 @@ def cart_update(request):
     product_id = 1
     product_obj = Product.objects.get(id=product_id)
     cart_obj, new_obj = Cart.objects.new_or_get(request)
-    cart_obj.products.add(product_obj)  # cart_obj.products.add(product_id)
+    # cart_obj.products.add(product_obj)  # cart_obj.products.add(product_id)
     if product_obj in cart_obj.products.all():
         cart_obj.products.remove(product_obj)
     else:

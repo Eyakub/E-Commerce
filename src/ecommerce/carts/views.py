d657e2cdb2c5501ebf53bbb08c71170e51339ac6
Eyakub Sorkar <eyakub.tryonyx@gmail.com>
2019-05-08 15:32:02 +0600

    add to cart error, not working perfectly

--- src/ecommerce/carts/views.py
+++ src/ecommerce/carts/views.py
@@ -42,14 +42,18 @@ def cart_home(request):
 
 
 def cart_update(request):
-    product_id = 1
-    product_obj = Product.objects.get(id=product_id)
-    cart_obj, new_obj = Cart.objects.new_or_get(request)
-    # cart_obj.products.add(product_obj)  # cart_obj.products.add(product_id)
-    if product_obj in cart_obj.products.all():
-        cart_obj.products.remove(product_obj)
-    else:
-        cart_obj.products.add(product_obj)
+    print(request.POST)
+    product_id = request.POST.get('product_id')
+    if product_id is not None:
+        try:
+            product_obj = Product.objects.get(id=product_id)
+        except Product.DoesNotExist:
+            print('Show message to user, product is gone?')
+            return redirect('cart:home')
+        cart_obj, new_obj = Cart.objects.new_or_get(request)
+        if product_obj in cart_obj.products.all():
+            cart_obj.products.remove(product_obj)
+        else:
+            cart_obj.products.add(product_obj)
 
-    # return redirect(product_obj.get_absolute_url())
     return redirect('cart:home')
